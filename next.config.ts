eee9f2d86d600b07b0a67e174fa907c6e8b48e85
K-avya7 <kavya1507.agr@gmail.com>
2026-02-26 14:47:36 +0530

    config files changed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  basePath: "/kavya-portfolio",
+  assetPrefix: "/kavya-portfolio/",
+
   images: {
     unoptimized: true,
   },
